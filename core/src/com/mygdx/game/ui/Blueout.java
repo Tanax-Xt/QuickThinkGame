5ee7aaa17614fded9f6d678365e3771c57cad3c8
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-07-08 19:26:01 +0300

    Blue screen for the "Game Over" was added on the 8th of July

---
+++ core/src/com/mygdx/game/ui/Blueout.java
@@ -0,0 +1,27 @@
+package com.mygdx.game.ui;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.ui.UiComponent;
+import com.mygdx.game.utils.GameSettings;
+
+public class Blueout extends UiComponent {
+
+    Texture blueoutTexture;
+
+    public Blueout() {
+        super(0, 0, GameSettings.SCR_WIDTH, GameSettings.SCR_HEIGHT);
+        Pixmap pixmap = new Pixmap((int) width, (int) height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(0, 75, 100, 0.33f);
+        pixmap.fill();
+        blueoutTexture = new Texture(pixmap);
+    }
+
+    @Override
+    public void draw(SpriteBatch spriteBatch) {
+        spriteBatch.draw(blueoutTexture, x, y, width, height);
+    }
+}
