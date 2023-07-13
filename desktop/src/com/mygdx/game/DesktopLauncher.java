89595a76f9dcb5cae20f9d9750005e9812d3a2bd
Danila <sedelnikovdanila@gmail.com>
2023-07-13 16:52:54 +0300

    update for pc

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("QuickThink");
+		config.setWindowedMode(600, 1080);
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
